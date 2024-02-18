39a45f581a18b1f1a6b2a272b47fcfff65069992
sherryhere99 <152762782+sherryhere99@users.noreply.github.com>
2024-02-18 14:54:37 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,16 +10,16 @@
 
 
 
-Project Name : ZUSYCO - MD
+Project Name : SHERRY-MD 
 Creator : @darkmakerofc , @mr-nima-x 
 
 */
 
 module.exports = {
-  OWNER_NUMBER: "94715166712",
-  SUDO_NUMBERS : "94715166712,94719574492",
-  OWNER_NAME: "『 𝙼𝚁 𝙽𝙸𝙼𝙰 』",
-  BOT_NAME : "ＺＵＳＹＣＯ",
+  OWNER_NUMBER: "923493750369",
+  SUDO_NUMBERS : "923493750369",
+  OWNER_NAME: "『 𝙼𝚁 SHERRY-X 』",
+  BOT_NAME : "𓄂≛⃝لاڈلا مرزا",
   ZUSYCO_FILE : "ZUSYCO-QR",
   LOGOS: {
     ALIVE: 'https://i.ibb.co/YTWznr9/ZUSYCO-ALIVE-IMAGE-LOWQ.jpg',
@@ -36,25 +36,25 @@ module.exports = {
   WORK_MODE : "onlygroups",
   WORK_MODE_MESSAGE_SEND : false,
   CAPTION : '© ɢᴇɴᴇʀᴀᴛᴇᴅ ʙʏ ᴢᴜꜱʏᴄᴏ ᴍᴅ ʙᴏᴛ',
-  STICKER_PACK : '[ ZUSYCO ]',
+  STICKER_PACK : '[ SHERRY-X ]',
   BAD_WORDS : 'fuck,pussy',
   ANTI_BAD : false,
   ANTI_LINKS : 'chat.whatsapp.com',
-  ANTI_LINK : false,
-  ANTI_LINK_KICK : false,
+  ANTI_LINK : true ,
+  ANTI_LINK_KICK : true ,
   MAX_SIZE : 100,
   MOROCCO_NUMBERS : {
-        BLOCK : false,
+        BLOCK : true ,
         NOT_WORKING : false,
   },
   READ : {
      ALL_MESSAGES : true,
      ONLY_COMMANDS : false,
-     STATUS : false,
+     STATUS : true ,
   },
-  BOT_OFFLINE : false,
+  BOT_OFFLINE : false ,
   INBOX_BLOCK : false,
-  AUTO_REACT : false,
+  AUTO_REACT : true ,
   OWNER_REACT : '🤵‍♂️',
   AUTO_BGM : false,
   BGM : {
@@ -72,7 +72,7 @@ module.exports = {
     "thugs" : "https://github.com/MR-NIMA-X/ZUSYCO-DB/raw/main/media/audios/ZUSYCO-thugs.mp3",
 
   },
-  AUTO_STICKER : false,
+  AUTO_STICKER : true ,
   STICKERS : {
     "pit" : "https://i.ibb.co/NVLMQDP/pit.webp",
     "agei" : "https://i.ibb.co/yqk8QQ9/agei.webp",
